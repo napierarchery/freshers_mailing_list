04a48ea7fd32d561a9e30ed5915a4b359164375e
napierarchery <36684152+napierarchery@users.noreply.github.com>
2018-08-30 22:02:38 +0100

    Upload of fully functional program
    Software can be used as is. Executable program found in 'NUAC Freshers Mailing List/bin/Debug' folder

---
+++ NUAC Freshers Mailing List/Form1.Designer.vb
@@ -0,0 +1,288 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.OutputBox1 = New System.Windows.Forms.TextBox()
+        Me.InputBox1 = New System.Windows.Forms.TextBox()
+        Me.InputBox2 = New System.Windows.Forms.TextBox()
+        Me.InputBox3 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.Button1.Location = New System.Drawing.Point(351, 477)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(125, 45)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Submit"
+        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.Visible = False
+        '
+        'OutputBox1
+        '
+        Me.OutputBox1.Location = New System.Drawing.Point(176, 437)
+        Me.OutputBox1.Name = "OutputBox1"
+        Me.OutputBox1.Size = New System.Drawing.Size(757, 20)
+        Me.OutputBox1.TabIndex = 1
+        Me.OutputBox1.TabStop = False
+        Me.OutputBox1.Visible = False
+        '
+        'InputBox1
+        '
+        Me.InputBox1.BackColor = System.Drawing.Color.Beige
+        Me.InputBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.InputBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!)
+        Me.InputBox1.Location = New System.Drawing.Point(174, 229)
+        Me.InputBox1.Name = "InputBox1"
+        Me.InputBox1.Size = New System.Drawing.Size(539, 35)
+        Me.InputBox1.TabIndex = 1
+        Me.InputBox1.Text = "PROGRAM NOT INITIALISED"
+        Me.InputBox1.Visible = False
+        '
+        'InputBox2
+        '
+        Me.InputBox2.BackColor = System.Drawing.Color.Beige
+        Me.InputBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.InputBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!)
+        Me.InputBox2.Location = New System.Drawing.Point(174, 307)
+        Me.InputBox2.Name = "InputBox2"
+        Me.InputBox2.Size = New System.Drawing.Size(539, 35)
+        Me.InputBox2.TabIndex = 2
+        Me.InputBox2.Text = "PROGRAM NOT INITIALISED"
+        Me.InputBox2.Visible = False
+        '
+        'InputBox3
+        '
+        Me.InputBox3.BackColor = System.Drawing.Color.Beige
+        Me.InputBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.InputBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!)
+        Me.InputBox3.Location = New System.Drawing.Point(174, 385)
+        Me.InputBox3.Name = "InputBox3"
+        Me.InputBox3.Size = New System.Drawing.Size(539, 35)
+        Me.InputBox3.TabIndex = 3
+        Me.InputBox3.Text = "PROGRAM NOT INITIALISED"
+        Me.InputBox3.Visible = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!)
+        Me.Label1.Location = New System.Drawing.Point(171, 197)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(78, 29)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Name"
+        Me.Label1.Visible = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!)
+        Me.Label2.Location = New System.Drawing.Point(171, 275)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(242, 29)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Matriculation Number"
+        Me.Label2.Visible = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!)
+        Me.Label3.Location = New System.Drawing.Point(171, 353)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(176, 29)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Personal Email"
+        Me.Label3.Visible = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.Beige
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(216, 640)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(160, 26)
+        Me.TextBox1.TabIndex = 8
+        Me.TextBox1.TabStop = False
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Button2.Location = New System.Drawing.Point(216, 678)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(105, 34)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "Initialise"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(216, 549)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(158, 20)
+        Me.TextBox2.TabIndex = 10
+        Me.TextBox2.Visible = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.Label4.Location = New System.Drawing.Point(213, 593)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(287, 34)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Enter a group name for this mailing list" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "then press Initiaslise to begin data re" &
+    "cording"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.ErrorImage = Nothing
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.InitialImage = Nothing
+        Me.PictureBox1.Location = New System.Drawing.Point(757, 104)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(560, 560)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 12
+        Me.PictureBox1.TabStop = False
+        Me.PictureBox1.Visible = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!)
+        Me.Label5.Location = New System.Drawing.Point(197, 104)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(496, 46)
+        Me.Label5.TabIndex = 15
+        Me.Label5.Text = "Napier Archery Mailing List"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(201, 166)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(644, 380)
+        Me.Label6.TabIndex = 17
+        Me.Label6.Text = resources.GetString("Label6.Text")
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!)
+        Me.Label7.Location = New System.Drawing.Point(135, 595)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(473, 52)
+        Me.Label7.TabIndex = 18
+        Me.Label7.Text = resources.GetString("Label7.Text")
+        Me.Label7.Visible = False
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(404, 646)
+        Me.CheckBox1.Margin = New System.Windows.Forms.Padding(2)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(144, 17)
+        Me.CheckBox1.TabIndex = 19
+        Me.CheckBox1.Text = "Record Personal Emails?"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.BackColor = System.Drawing.SystemColors.Window
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.ClientSize = New System.Drawing.Size(1109, 630)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.InputBox3)
+        Me.Controls.Add(Me.InputBox2)
+        Me.Controls.Add(Me.InputBox1)
+        Me.Controls.Add(Me.OutputBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.Text = "NapierArchery Freshers Mailing List"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents OutputBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents InputBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents InputBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents InputBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+
+End Class
+
---
+++ NUAC Freshers Mailing List/Form1.vb
@@ -0,0 +1,101 @@
+﻿Public Class Form1
+
+
+    'Heading string is first line of csv document, contains all fields needed for gmail compatibility
+    Dim headingString As String = "Time&Date,Matriculation Number,Name,Given Name,Additional Name,Family Name,Yomi Name,Given Name Yomi,Additional Name Yomi,Family Name Yomi,Name Prefix,Name Suffix,Initials,Nickname,Short Name,Maiden Name,Birthday,Gender,Location,Billing Information,Directory Server,Mileage,Occupation,Hobby,Sensitivity,Priority,Subject,Notes,Group Membership,E-mail 1 - Type,E-mail 1 - Value,E-mail 2 - Type,E-mail 2 - Value"
+    Dim ShowPersonalEmail As Boolean = False
+
+
+    'Initiasise program for use and enter group name'
+    Private Sub button2_click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+
+        TextBox2.Text = headingString & vbCrLf
+
+        My.Computer.FileSystem.CreateDirectory("C:\NapierArchery\")
+        My.Computer.FileSystem.WriteAllText("C:\NapierArchery\Mailinglist.csv", TextBox2.Text, True)
+
+        If CheckBox1.Checked Then
+            ShowPersonalEmail = True
+        End If
+
+        hideInitFields()
+        readyDataRecording()
+
+    End Sub
+
+
+
+    ' Reveals personal email field if checkbox checked in Initialisation
+    Private Sub PersonalEmail(ByVal ShowPersonalEmail As Boolean)
+        If ShowPersonalEmail = True Then
+            Label3.Show()   'Show label Personal Email
+            InputBox3.Show() 'Personal email field
+        End If
+    End Sub
+
+
+
+    'User entered data saved as gmail compatible csv file
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+
+        OutputBox1.Text = Date.Now & "," & InputBox2.Text & "," & InputBox1.Text & "," & "," & "," & "," & "," & "," & "," & "," & "," & "," & "," & "," & "," & "," & "," & "," & "," & "," & "," & "," & "," & "," & "," & "," & "," & InputBox2.Text & "," & TextBox1.Text & "," & "* Home" & "," & InputBox3.Text & "," & "* Uni" & "," & InputBox2.Text & "@live.napier.ac.uk" & vbCrLf
+
+
+        My.Computer.FileSystem.WriteAllText("C:\NapierArchery\Mailinglist.csv", OutputBox1.Text, True)
+
+
+        'Display info submitted for user to review
+        If ShowPersonalEmail = True Then
+
+            MessageBox.Show("Information Submitted" & vbCrLf & vbCrLf & "Name : " & vbTab & InputBox1.Text & vbCrLf &
+                        "Matric : " & vbTab & InputBox2.Text & vbCrLf &
+                        "Email : " & vbTab & InputBox3.Text & vbCrLf & vbCrLf &
+                        "If you have made a mistake, just re-enter your details")
+
+        Else
+            MessageBox.Show("Information Submitted" & vbCrLf & vbCrLf & "Name : " & vbTab & InputBox1.Text & vbCrLf &
+                        "Matric : " & vbTab & InputBox2.Text & vbCrLf & vbCrLf &
+                        "If you have made a mistake, just re-enter your details")
+
+        End If
+
+
+        'ready fields for next user
+        readyDataRecording()
+
+
+    End Sub
+
+
+
+    ' Prepare form for user input
+    Private Sub readyDataRecording()
+        InputBox1.Text = ""
+        InputBox2.Text = ""
+        InputBox3.Text = ""
+
+        PictureBox1.Show() 'Shows logo
+        Label1.Show()
+        Label2.Show()
+        Label7.Show()
+        InputBox1.Show()
+        InputBox2.Show()
+        PersonalEmail(ShowPersonalEmail)
+
+        Button1.Show()  'Submit Details Button
+        InputBox1.Focus() 'Select Name Field for next user
+
+    End Sub
+
+
+    Private Sub hideInitFields()
+        Label6.Hide()   'Hide Instructions
+        Button2.Hide()  'Init Button
+        Label4.Hide()   'Info text for Initialisation
+        TextBox1.Hide() 'MailingList Group Name field
+        CheckBox1.Hide() 'Hide Personal Email checkbox
+
+    End Sub
+
+End Class
